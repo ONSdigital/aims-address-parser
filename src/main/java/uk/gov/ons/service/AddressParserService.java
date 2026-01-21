d2378f689c71a8a19b9377c68d0cacbdbdfc6858
RichardSmithONS <richard.m.smith@ons.gov.uk>
2026-01-21 12:04:33 +0000

    temporarily rollback to SB 3.5.9

--- src/main/java/uk/gov/ons/service/AddressParserService.java
+++ src/main/java/uk/gov/ons/service/AddressParserService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Service;
 
-import tools.jackson.databind.json.JsonMapper;
+// alternative import for Spring Boot 4.
+//import tools.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import scala.collection.JavaConverters;
 import uk.gov.ons.addressIndex.crfscala.CrfScalaJniImpl;
@@ -24,8 +26,9 @@ public class AddressParserService {
 	}
 
 	public Tokens parseAddress(String address) {
-		
-		return new JsonMapper()
+		return new ObjectMapper()
+// alternative class for Spring Boot 4
+//		return new JsonMapper()
 				.convertValue(JavaConverters.mapAsJavaMapConverter(new Parser(natif).parse(address)).asJava(), Tokens.class);
 	}
 
